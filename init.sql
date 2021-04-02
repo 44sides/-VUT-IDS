a7b653f9ba40342863273383ee7990eccb8a2620
mfindra <miskofindra@gmail.com>
2021-04-02 15:02:19 +0200

    create sql tables

---
+++ init.sql
@@ -0,0 +1,10 @@
+-- clear table to prevent duplicate values
+DELETE FROM uzivatel ;
+
+
+-- initilize values
+INSERT INTO uzivatel VALUES (DEFAULT, 'Jan', 'Novák','123456789', 'Mlynská 12 Praha', '4206969696969');
+
+
+
+
